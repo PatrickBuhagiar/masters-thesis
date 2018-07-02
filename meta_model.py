78cc96ba11e898837b65557779a7192f26d51fe4
Patrick Buhagiar <patrickbhgr@gmail.com>
2018-07-02 12:18:26 +0100

    code cleanup

--- meta_model.py
+++ meta_model.py
@@ -1,19 +1,20 @@
 import pandas as pd
+import tensorflow as tf
 from pymongo import MongoClient
 
+from best_architecture import tf_confusion_metrics
 from stock_model import load_data, divide_into_training_testing, organise_data, extract_market_directions, log_diff, \
-    get_model_predictions, load_model
+    get_model_predictions
+from toolbox import load_macroeconomic_data, get_lagged_macroeconomic_data, extract_macroeconomic_data, \
+    prepare_macroeconomic_data
 
-# MongoDB
-from toolbox import load_macroeconomic_data, get_lagged_macroeconomic_data, extract_macroeconomic_data
-
+# MongoDB initialisation
 client = MongoClient('localhost', 27017)
 db = client['thesis']
 
+# Load the stock data that will be used for training and testing the meta model. 
 start = pd.datetime(2014, 0o1, 0o1)
 end = pd.datetime(2018, 0o1, 0o1)
-
-# Load data
 df = load_data(start, end)
 log_return_data = log_diff(df)
 extract_market_directions(log_return_data)
@@ -22,119 +23,82 @@ training_test_data = organise_data(log_return_data)
 # Split data into training and testing
 inputs = training_test_data[training_test_data.columns[2:]]
 outputs = training_test_data[training_test_data.columns[:2]]
-test_outputs, test_inputs, training_outputs, training_inputs = divide_into_training_testing(inputs, outputs,
-                                                                                            len(training_test_data))
 
 # get all stored models and get predictions
 cursor = db.posts.find({})  # get all items
-dates = []
 meta_inputs = pd.DataFrame()
-range = pd.date_range(start, end)
-meta_inputs['Date'] = range[:training_inputs.index.size]
+_range = pd.date_range(start, end)
+meta_inputs['Date'] = _range[:inputs.index.size]
 meta_inputs.set_index('Date')
-model_predictions = {}
 
 for item in cursor:
     date = item.get("_id")
-    dates.append(date)
     print(date)
-    model_predictions[date + '_predictions'] = get_model_predictions(date, training_inputs)
-    meta_inputs[date + "_predictions_0"] = model_predictions[date + '_predictions'][:, 0]
-    meta_inputs[date + "_predictions_1"] = model_predictions[date + '_predictions'][:, 1]
+    model_predictions = get_model_predictions(date, inputs)
+    meta_inputs[date + "_predictions_0"] = model_predictions[:, 0]
+    meta_inputs[date + "_predictions_1"] = model_predictions[:, 1]
+    # meta_inputs.ix[model_predictions[:,0] >= 0.5, date + "_prediction"] = 1
 
 
 # Load all macroeconomic data
-trade_balance_data = load_macroeconomic_data("data/macroeconomics/GB/GB_BALANCE_OF_TRADE.csv", 90)
-gdp_data = load_macroeconomic_data("data/macroeconomics/GB/GB_GDP.csv", 90)
-inflation_data = load_macroeconomic_data("data/macroeconomics/GB/GB_INFLATION.csv", 22)
-interest_data = extract_macroeconomic_data("data/macroeconomics/GB/GB_INTRST.csv", 1, start - pd.DateOffset(years=2), end, 'D')
-unemployment_data = load_macroeconomic_data("data/macroeconomics/GB/GB_UNEMPLOYMENT.csv", 60)
-
-tbd_0 = []
-tbd_1 = []
-tbd_2 = []
-tbd_3 = []
-
-gdp_0 = []
-gdp_1 = []
-gdp_2 = []
-gdp_3 = []
-
-inf_0 = []
-inf_1 = []
-inf_2 = []
-inf_3 = []
-
-int_0 = []
-int_1 = []
-int_2 = []
-int_3 = []
-
-uem_0 = []
-uem_1 = []
-uem_2 = []
-uem_3 = []
-
-for row in meta_inputs.iterrows():
-    date = row[1].Date
-    tbd = get_lagged_macroeconomic_data(trade_balance_data, date)
-    tbd_0.append(tbd[0])
-    tbd_1.append(tbd[1])
-    tbd_2.append(tbd[2])
-    tbd_3.append(tbd[3])
-
-    gdp = get_lagged_macroeconomic_data(gdp_data, date)
-    gdp_0.append(gdp[0])
-    gdp_1.append(gdp[1])
-    gdp_2.append(gdp[2])
-    gdp_3.append(gdp[3])
-
-    inf = get_lagged_macroeconomic_data(inflation_data, date)
-    inf_0.append(inf[0])
-    inf_1.append(inf[1])
-    inf_2.append(inf[2])
-    inf_3.append(inf[3])
-
-    int = get_lagged_macroeconomic_data(interest_data, date, type='D')
-    int_0.append(int[0])
-    int_1.append(int[1])
-    int_2.append(int[2])
-    int_3.append(int[3])
-
-    uem = get_lagged_macroeconomic_data(unemployment_data, date)
-    uem_0.append(uem[0])
-    uem_1.append(uem[1])
-    uem_2.append(uem[2])
-    uem_3.append(uem[3])
-
-meta_inputs['trade_balance_data_0'] = tbd_0
-meta_inputs['trade_balance_data_1'] = tbd_1
-meta_inputs['trade_balance_data_2'] = tbd_2
-meta_inputs['trade_balance_data_3'] = tbd_3
-
-meta_inputs['gdp_data_0'] = gdp_0
-meta_inputs['gdp_data_1'] = gdp_1
-meta_inputs['gdp_data_2'] = gdp_2
-meta_inputs['gdp_data_3'] = gdp_3
-
-meta_inputs['inflation_data_0'] = inf_0
-meta_inputs['inflation_data_1'] = inf_1
-meta_inputs['inflation_data_2'] = inf_2
-meta_inputs['inflation_data_3'] = inf_3
-
-meta_inputs['interest_data_0'] = int_0
-meta_inputs['interest_data_1'] = int_1
-meta_inputs['interest_data_2'] = int_2
-meta_inputs['interest_data_3'] = int_3
-
-meta_inputs['unemployment_data_0'] = uem_0
-meta_inputs['unemployment_data_1'] = uem_1
-meta_inputs['unemployment_data_2'] = uem_2
-meta_inputs['unemployment_data_3'] = uem_3
+prepare_macroeconomic_data(start, end, meta_inputs)
 
 meta_inputs = meta_inputs.set_index('Date')
 # concatenate all model outputs and macro data into one matrix + expected output
 
+
+# divide into training and testing
+test_outputs, test_inputs, training_outputs, training_inputs = divide_into_training_testing(meta_inputs, outputs,
+                                                                                            len(meta_inputs))
+
 # build meta model
+sess = tf.Session()
+n_hidden_nodes = 45
+num_predictors = len(training_inputs.columns)
+num_classes = len(training_outputs.columns)
+feature_data = tf.placeholder("float", [None, num_predictors], name="feature_data")
+actual_classes = tf.placeholder("float", [None, num_classes], name="actual_classes")
+
+weights1 = tf.Variable(tf.truncated_normal([num_predictors, n_hidden_nodes], stddev=0.0001))
+biases1 = tf.Variable(tf.ones([n_hidden_nodes]))
+weights2 = tf.Variable(tf.truncated_normal([n_hidden_nodes, num_classes], stddev=0.0001))
+biases2 = tf.Variable(tf.ones([2]))
+
+hidden_layer = tf.nn.relu(tf.matmul(feature_data, weights1) + biases1)
+model = tf.nn.softmax(tf.matmul(hidden_layer, weights2) + biases2)
+cost = -tf.reduce_sum(actual_classes * tf.log(model))
+train_op1 = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
+init = tf.global_variables_initializer()
+
+# Run Model
+sess.run(init)
+correct_prediction = tf.equal(tf.argmax(model, 1), tf.argmax(actual_classes, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+for i in range(1, 30001):
+    sess.run(
+        train_op1,
+        feed_dict={
+            feature_data: training_inputs.values,
+            actual_classes: training_outputs.values.reshape(len(training_outputs.values), 2)
+        }
+    )
+    # Every 5000, we are going to print the current training accuracy of the model
+    if i % 5000 == 0:
+        print("start date", start, "iteration", i, sess.run(
+            accuracy,
+            feed_dict={
+                feature_data: training_inputs.values,
+                actual_classes: training_outputs.values.reshape(len(training_outputs.values), 2)
+            }
+        ))
+
+feed_dict = {
+    feature_data: test_inputs.values,
+    actual_classes: test_outputs.values.reshape(len(test_outputs.values), 2)
+}
+
+# Calculate the F1 Score and Accuracy with the training set
+f1_score, accuracy = tf_confusion_metrics(model, actual_classes, sess, feed_dict)
+print(start, f1_score, accuracy)
 
 # execute model
--- toolbox.py
+++ toolbox.py
@@ -422,3 +422,84 @@ def make_time_series_stationary(ts):
     diff = ts_log - ts_log.shift()
     diff.dropna(inplace=True)
     return diff
+
+
+def prepare_macroeconomic_data(start, end, meta_inputs):
+    trade_balance_data = load_macroeconomic_data("data/macroeconomics/GB/GB_BALANCE_OF_TRADE.csv", 90)
+    gdp_data = load_macroeconomic_data("data/macroeconomics/GB/GB_GDP.csv", 90)
+    inflation_data = load_macroeconomic_data("data/macroeconomics/GB/GB_INFLATION.csv", 22)
+    interest_data = extract_macroeconomic_data("data/macroeconomics/GB/GB_INTRST.csv", 1,
+                                               start - pd.DateOffset(years=2),
+                                               end, 'D')
+    unemployment_data = load_macroeconomic_data("data/macroeconomics/GB/GB_UNEMPLOYMENT.csv", 60)
+    tbd_0 = []
+    tbd_1 = []
+    tbd_2 = []
+    tbd_3 = []
+    gdp_0 = []
+    gdp_1 = []
+    gdp_2 = []
+    gdp_3 = []
+    inf_0 = []
+    inf_1 = []
+    inf_2 = []
+    inf_3 = []
+    int_0 = []
+    int_1 = []
+    int_2 = []
+    int_3 = []
+    uem_0 = []
+    uem_1 = []
+    uem_2 = []
+    uem_3 = []
+    for row in meta_inputs.iterrows():
+        date = row[1].Date
+        tbd = get_lagged_macroeconomic_data(trade_balance_data, date)
+        tbd_0.append(tbd[0])
+        tbd_1.append(tbd[1])
+        tbd_2.append(tbd[2])
+        tbd_3.append(tbd[3])
+
+        gdp = get_lagged_macroeconomic_data(gdp_data, date)
+        gdp_0.append(gdp[0])
+        gdp_1.append(gdp[1])
+        gdp_2.append(gdp[2])
+        gdp_3.append(gdp[3])
+
+        inf = get_lagged_macroeconomic_data(inflation_data, date)
+        inf_0.append(inf[0])
+        inf_1.append(inf[1])
+        inf_2.append(inf[2])
+        inf_3.append(inf[3])
+
+        int_ = get_lagged_macroeconomic_data(interest_data, date, type='D')
+        int_0.append(int_[0])
+        int_1.append(int_[1])
+        int_2.append(int_[2])
+        int_3.append(int_[3])
+
+        uem = get_lagged_macroeconomic_data(unemployment_data, date)
+        uem_0.append(uem[0])
+        uem_1.append(uem[1])
+        uem_2.append(uem[2])
+        uem_3.append(uem[3])
+    meta_inputs['trade_balance_data_0'] = tbd_0
+    meta_inputs['trade_balance_data_1'] = tbd_1
+    meta_inputs['trade_balance_data_2'] = tbd_2
+    meta_inputs['trade_balance_data_3'] = tbd_3
+    meta_inputs['gdp_data_0'] = gdp_0
+    meta_inputs['gdp_data_1'] = gdp_1
+    meta_inputs['gdp_data_2'] = gdp_2
+    meta_inputs['gdp_data_3'] = gdp_3
+    meta_inputs['inflation_data_0'] = inf_0
+    meta_inputs['inflation_data_1'] = inf_1
+    meta_inputs['inflation_data_2'] = inf_2
+    meta_inputs['inflation_data_3'] = inf_3
+    meta_inputs['interest_data_0'] = int_0
+    meta_inputs['interest_data_1'] = int_1
+    meta_inputs['interest_data_2'] = int_2
+    meta_inputs['interest_data_3'] = int_3
+    meta_inputs['unemployment_data_0'] = uem_0
+    meta_inputs['unemployment_data_1'] = uem_1
+    meta_inputs['unemployment_data_2'] = uem_2
+    meta_inputs['unemployment_data_3'] = uem_3
