f332f370fe181f79e9a62e81f3af7917d327b3a0
PatrickBuhagiar <patrickbhgr@gmail.com>
2018-08-21 13:14:16 +0100

    commented out plotting stuff due to GCloud env

--- src/only_other_stock_markets.py
+++ src/only_other_stock_markets.py
@@ -1,6 +1,6 @@
-import matplotlib.pylab as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
+#import matplotlib.pylab as plt
+#from mpl_toolkits.mplot3d import Axes3D
+#from matplotlib import cm
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -328,15 +328,15 @@ if __name__ == '__main__':
     # uncomment this to read from file
     # accuracies = np.array(list(csv.reader(open("other_stocks_accuracies.csv"), delimiter=","))).astype("float")
     # f1s = np.array(list(csv.reader(open("other_stocks_f1s.csv"), delimiter=","))).astype("float")
-    Y, X = np.meshgrid(Y, X)
+  #  Y, X = np.meshgrid(Y, X)
 
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.set_xlabel("Learning Rate")
-    ax.set_ylabel("Number of Hidden Layer Nodes")
-    ax.set_zlabel("F1 Score")
+ #   fig = plt.figure()
+ #   ax = fig.gca(projection='3d')
+  #  ax.set_xlabel("Learning Rate")
+   # ax.set_ylabel("Number of Hidden Layer Nodes")
+ #   ax.set_zlabel("F1 Score")
 
-    plt.title("3D plot of Number of Nodes VS Learning Rate VS F1 Score")
-    surf = ax.plot_surface(Y, X, accuracies, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0)
-    surf = ax.plot_surface(Y, X, f1s, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0)
-    plt.show()
+    #plt.title("3D plot of Number of Nodes VS Learning Rate VS F1 Score")
+    #surf = ax.plot_surface(Y, X, accuracies, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0)
+    #surf = ax.plot_surface(Y, X, f1s, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0)
+    #plt.show()
