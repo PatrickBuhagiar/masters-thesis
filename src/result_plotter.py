dca0b6c46db46a4df9c5317ddade2337a5694e08
Patrick Buhagiar <patrickbhgr@gmail.com>
2018-08-30 17:54:22 +0100

    new params

--- src/result_plotter.py
+++ src/result_plotter.py
@@ -5,12 +5,12 @@ import csv
 import numpy as np
 
 
-X = np.arange(5, 11, 1)  # number of nodes
-Y = np.arange(0.0005, 0.0021, 0.0001)  # learning rates
+X = np.arange(30, 36, 1)  # number of nodes
+Y = np.arange(0.006, 0.02, 0.002)  # learning rates
 
-accuracies = np.array(list(csv.reader(open("base_out/correct/base_accuracies_5-11_0005-0021.csv"), delimiter=","))).astype(
+accuracies = np.array(list(csv.reader(open("h2_accuracies_26-36_002-007.csv"), delimiter=","))).astype(
     "float")
-f1s = np.array(list(csv.reader(open("base_out/correct/base_f1s_5-11_0005-0021.csv"), delimiter=","))).astype("float")
+f1s = np.array(list(csv.reader(open("h2_f1s_26-36_002-007.csv"), delimiter=","))).astype("float")
 Y, X = np.meshgrid(Y, X)
 
 fig = plt.figure()
@@ -19,10 +19,7 @@ ax.set_xlabel("Learning Rate")
 ax.set_ylabel("Number of Hidden Layer Nodes")
 ax.set_zlabel("Accuracy")
 
-max_z=max(accuracies)
-index_max_z=accuracies.argmax()
-
-plt.title("Base Case - 3D plot for Number of Nodes VS Learning Rate VS Accuracy")
+plt.title("H2 - 3D plot for Number of Nodes VS Learning Rate VS Accuracy")
 surf = ax.plot_surface(Y, X, accuracies, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0, label="accuracy", antialiased=False)
-# surf = ax.plot_surface(Y, X, f1s, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0, label="f1")
+surf = ax.plot_surface(Y, X, f1s, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0, label="f1")
 plt.show()
