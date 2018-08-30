6f5cbad1463e9e0c9f8fa2fc3f223abca789f8da
Patrick Buhagiar <patrickbhgr@gmail.com>
2018-08-30 09:14:31 +0100

    removed models because no meta

--- src/result_plotter.py
+++ src/result_plotter.py
@@ -4,21 +4,25 @@ from matplotlib import cm
 import csv
 import numpy as np
 
-X = np.arange(20, 41, 5)  # number of nodes
-Y = np.arange(0.001, 0.011, 0.0005)  # learning rates
 
-accuracies = np.array(list(csv.reader(open("h2_accuracies_20-41_001-011.csv"), delimiter=","))).astype(
+X = np.arange(5, 11, 1)  # number of nodes
+Y = np.arange(0.0005, 0.0021, 0.0001)  # learning rates
+
+accuracies = np.array(list(csv.reader(open("base_out/correct/base_accuracies_5-11_0005-0021.csv"), delimiter=","))).astype(
     "float")
-f1s = np.array(list(csv.reader(open("h2_f1s_20-41_001-011.csv"), delimiter=","))).astype("float")
+f1s = np.array(list(csv.reader(open("base_out/correct/base_f1s_5-11_0005-0021.csv"), delimiter=","))).astype("float")
 Y, X = np.meshgrid(Y, X)
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 ax.set_xlabel("Learning Rate")
 ax.set_ylabel("Number of Hidden Layer Nodes")
-ax.set_zlabel("F1 Score")
+ax.set_zlabel("Accuracy")
 
-plt.title("3D plot of Number of Nodes VS Learning Rate VS F1 Score")
-surf = ax.plot_surface(Y, X, accuracies, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0)
-surf = ax.plot_surface(Y, X, f1s, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0)
+max_z=max(accuracies)
+index_max_z=accuracies.argmax()
+
+plt.title("Base Case - 3D plot for Number of Nodes VS Learning Rate VS Accuracy")
+surf = ax.plot_surface(Y, X, accuracies, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0, label="accuracy", antialiased=False)
+# surf = ax.plot_surface(Y, X, f1s, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0, label="f1")
 plt.show()
