d77cf56e58f86137f4e196d471365640add25c2e
Patrick Buhagiar <patrickbhgr@gmail.com>
2018-09-02 02:55:39 +0100

    new params

--- src/h2_stage_1.py
+++ src/h2_stage_1.py
@@ -180,7 +180,7 @@ if __name__ == '__main__':
         ftse_data = load_data(date, date + pd.DateOffset(years=5))
         test_outputs, test_inputs, training_outputs, training_inputs = prepare_data(ftse_data)
         n_nodes = np.arange(4, 8, 1)  # number of nodes
-        learning_rates = np.arange(0.0003, 0.008, 0.0001)  # learning rates
+        learning_rates = np.arange(0.0003, 0.0008, 0.0001)  # learning rates
         futures.append(
             pool.submit(process, learning_rates, n_nodes, training_inputs, training_outputs,
                         test_inputs,
