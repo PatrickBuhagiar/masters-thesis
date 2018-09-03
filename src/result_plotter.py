1e0bf99c6b9585850b7efd18dd51e42d6fc3411f
Patrick Buhagiar <patrickbhgr@gmail.com>
2018-09-03 18:10:21 +0100

    new params h3

--- src/result_plotter.py
+++ src/result_plotter.py
@@ -4,12 +4,12 @@ from matplotlib import cm
 import csv
 import numpy as np
 
-X = np.arange(5, 11, 1)  # number of nodes
-Y = np.arange(0.0005, 0.0021, 0.0001)  # learning rates
+X = np.arange(25, 45, 2)  # number of nodes
+Y = np.arange(0.00001, 0.00011, 0.00002)  # learning rates
 
-accuracies = np.array(list(csv.reader(open("base_out/correct/base_accuracies_5-11_0005-0021.csv"), delimiter=","))).astype(
+accuracies = np.array(list(csv.reader(open("h3_accuracies_25-45_00001-0011.csv"), delimiter=","))).astype(
     "float")
-f1s = np.array(list(csv.reader(open("base_out/correct/base_f1s_5-11_0005-0021.csv"), delimiter=","))).astype("float")
+f1s = np.array(list(csv.reader(open("h3_f1s_25-45_00001-0011.csv"), delimiter=","))).astype("float")
 Y, X = np.meshgrid(Y, X)
 
 fig = plt.figure()
@@ -19,6 +19,6 @@ ax.set_ylabel("Number of Hidden Layer Nodes")
 ax.set_zlabel("F1 Score")
 
 plt.title("Base Case - 3D plot for Number of Nodes VS Learning Rate VS F1 Score")
-surf = ax.plot_surface(Y, X, accuracies, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0, label="accuracy", antialiased=False)
-surf = ax.plot_surface(Y, X, f1s, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0, label="f1")
+surf = ax.plot_surface(Y, X, accuracies, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0, label="accuracy")
+# surf = ax.plot_surface(Y, X, f1s, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0, label="f1")
 plt.show()
